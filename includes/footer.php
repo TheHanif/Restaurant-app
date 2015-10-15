eb74784bec1251fab4006697804b7dad13149795
Muhammad Hanif <thehanif@msn.com>
2015-10-15 20:45:21 +0500

    Added markups
    Added sidebar markup
    Added script

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+</div> <!-- .content-wrapper -->
+</main> <!-- .cd-main-content -->
+
+</div><!-- #main-wrapper -->
+
+<script src="//code.jquery.com/jquery.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+<script src="assets/codyhouse-menu/js/jquery.menu-aim.js"></script>
+<script src="assets/codyhouse-menu/js/main.js"></script> <!-- Resource jQuery -->
+</body>
+</html>
--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,90 @@
 <?php require_once 'common/init.php'; ?>
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
+
+	<!-- CODYHOUSE MENU -->
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="assets/codyhouse-menu/css/style.css"> <!-- Resource style -->
+	<script src="assets/codyhouse-menu/js/modernizr.js"></script> <!-- Modernizr -->
+
+	<link rel="stylesheet" href="assets/css/main-styles.css">
+
+</head>
+<body>
+
+<div id="main-wrapper">
+
+<header class="cd-main-header">
+	<a href="#0" class="cd-logo"><img src="assets/codyhouse-menu/img/cd-logo.svg" alt="Logo"></a>
+
+	<div class="cd-search is-hidden">
+		<form action="#0">
+			<input type="search" placeholder="Search...">
+		</form>
+	</div> <!-- cd-search -->
+
+	<a href="#0" class="cd-nav-trigger"><span></span></a>
+
+	<nav class="cd-nav">
+		<ul class="cd-top-nav">
+			<li><a href="#0">Tour</a></li>
+			<li><a href="#0">Support</a></li>
+			<li class="has-children account">
+				<a href="#0">
+					<img src="assets/codyhouse-menu/img/cd-avatar.png" alt="avatar">
+					Account
+				</a>
+
+				<ul>
+
+					<li><a href="#0">My Account</a></li>
+					<li><a href="#0">Edit Account</a></li>
+					<li><a href="#0">Logout</a></li>
+				</ul>
+			</li>
+		</ul>
+	</nav>
+</header> <!-- .cd-main-header -->
+
+<main class="cd-main-content">
+	<nav class="cd-side-nav">
+		<ul>
+			<li class="has-children">
+				<a href="#0"><i class="fa fa-th"></i> Products</a>
+				
+				<ul>
+					<li><a href="#0">All Data</a></li>
+					<li><a href="#0">Category 1</a></li>
+					<li><a href="#0">Category 2</a></li>
+				</ul>
+			</li>
+			<li class="has-children active">
+				<a href="#0"><i class="fa fa-th"></i> Categories</a>
+				
+				<ul>
+					<li><a href="#0">All Notifications</a></li>
+					<li><a href="#0">Friends</a></li>
+					<li><a href="#0">Other</a></li>
+				</ul>
+			</li>
+
+			<li class="has-children">
+				<a href="#0"><i class="fa fa-th"></i> Special Offers</a>
+				
+				<ul>
+					<li><a href="#0">All Comments</a></li>
+					<li><a href="#0">Edit Comment</a></li>
+					<li><a href="#0">Delete Comment</a></li>
+				</ul>
+			</li>
+		</ul>
+	</nav>
+
+	<div class="content-wrapper">
