855f233495f33cfadeef299c81c996286a7682c3
Muhammad Hanif <thehanif@msn.com>
2015-10-21 12:54:46 +0500

    Package
    Added features markup and php code
    added help small help blocks

--- add_package.php
+++ add_package.php
@@ -18,23 +18,24 @@
 	$package = new package();
 	
 	$ID = (isset($_GET['id']))? $_GET['id'] : NULL;
-			if (isset($_POST['add_package'])) {		
-				
-				// Update old record
-				if (isset($ID)) {
-					$results = $package->update_package($_POST, $ID);
-				}else{ // Insert new
-					$results = $package->insert_package($_POST);
-				}
-				if ($results) {
-					echo '<div class="alert alert-success" role="alert">';  echo (isset($_GET['id']))? 'Updated ' : 'Added '; echo  'package Sucessfully </div>';
-				}else{
-					echo '<div class="alert alert-danger" role="alert"> Error </div>';
-				}
-			}
-			if (isset($ID)) {
-				$package_result = $package->get_packages($ID);
-			}
+	if (isset($_POST['add_package'])) {		
+		
+		// Update old record
+		if (isset($ID)) {
+			$results = $package->update_package($_POST, $ID);
+		}else{ // Insert new
+			$results = $package->insert_package($_POST);
+		}
+		if ($results) {
+			echo '<div class="alert alert-success" role="alert">';  echo (isset($_GET['id']))? 'Updated ' : 'Added '; echo  'package Sucessfully </div>';
+		}else{
+			echo '<div class="alert alert-danger" role="alert"> Error </div>';
+		}
+	}
+	if (isset($ID)) {
+		$package_result = $package->get_packages($ID);
+		$package_result->package_features = json_decode($package_result->package_features);
+	}
 	
 ?>
 
@@ -43,14 +44,15 @@
 		  <div class="form-group">
 		    <label for="package_unique_id" class="col-sm-3 control-label">ID</label>
 		    <div class="col-sm-8">
-		      <input type="text" class="form-control" name="package_unique_id" id="package_unique_id" value="<?php echo (isset($ID))? $package_result->package_unique_id : '123' ?>" placeholder="Id" readonly>
+		      <input type="text" class="form-control" name="package_unique_id" id="package_unique_id" value="<?php echo (isset($ID))? $package_result->package_unique_id : '' ?>" placeholder="Id" readonly>
+		      <small class="help-block">Unique Identifier</small>
 		    </div>
 		  </div>
 
 		  <div class="form-group">
 		    <label for="package_name" class="col-sm-3 control-label">Name</label>
 		    <div class="col-sm-8">
-		      <input type="text" class="form-control" name="package_name" id="package_name" value="<?php echo (isset($ID))? $package_result->package_name : '' ?>" placeholder="Name">
+		      <input type="text" class="form-control" name="package_name" id="package_name" value="<?php echo (isset($ID))? $package_result->package_name : '' ?>" placeholder="Small description">
 		    </div>
 		  </div>
 
@@ -60,6 +62,31 @@
 		      <textarea class="form-control" id="package_desc" name="package_desc" placeholder="Details"><?php echo (isset($ID))? $package_result->package_detail : '' ?></textarea>
 		    </div>
 		  </div>
+
+		  <div class="form-group">
+		    <label class="col-sm-3 control-label"></label>
+		    <div class="col-sm-8 packages-features">
+				<label for="" class="control-label"><strong>Features</strong></label>
+				
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Branches</label>
+					<div class="col-sm-8">
+						<input type="text" class="form-control" name="package_features[branches]" value="<?php echo (isset($ID))? $package_result->package_features->branches : '' ?>">
+					</div>
+				</div>
+
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Tables</label>
+					<div class="col-sm-8">
+						<input type="text" class="form-control" name="package_features[tables]" value="<?php echo (isset($ID))? $package_result->package_features->tables : '' ?>">
+						<small class="help-block">Number of table in each branch</small>
+					</div>
+				</div>
+
+		    </div><!-- // .packages-features -->
+		  </div>
+
+
 
 		  <div class="col-sm-offset-3 form-group btn-container ">
 			  <input type="submit" name="add_package" class="btn btn-primary" value="Save">
@@ -74,7 +101,7 @@
 		    <label for="package_status" class="col-sm-3 control-label">Status</label>
 		    <div class="col-sm-8">
 		      <select class="form-control" id="package_status" name="package_status"  required>
-					<?php foreach($status as $status_key => $status_value){ ?>
+					<?php foreach($package->packages_status as $status_key => $status_value){ ?>
 						<option value="<?php echo $status_key; ?>" <?php (isset($ID))? $selected_status = $package_result->package_status : '';if(isset($ID)){if($status_key == $selected_status){echo 'selected=selected';}}?> ><?php echo $status_value; ?></option>
 						<?php } ?>
 				</select>
